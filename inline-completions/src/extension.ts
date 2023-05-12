2b1575d5077e5ac9b30b018ac51d8d2648402aa0
Tom Scholtz <tom.scholtz@lifeq.com>
2023-05-12 16:12:15 +0200

    Revert "slim down to code actions sample"
    This reverts commit b86b7f5822896dae232f7743468621ad50686c1d.

---
+++ inline-completions/src/extension.ts
@@ -0,0 +1,81 @@
+import * as vscode from 'vscode';
+import { Range } from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	console.log('inline-completions demo started');
+	vscode.commands.registerCommand('demo-ext.command1', async (...args) => {
+		vscode.window.showInformationMessage('command1: ' + JSON.stringify(args));
+	});
+
+	const provider: vscode.InlineCompletionItemProvider = {
+		async provideInlineCompletionItems(document, position, context, token) {
+			console.log('provideInlineCompletionItems triggered');
+			const regexp = /\/\/ \[(.+?),(.+?)\)(.*?):(.*)/;
+			if (position.line <= 0) {
+				return;
+			}
+
+			const result: vscode.InlineCompletionList = {
+				items: [],
+				commands: [],
+			};
+
+			let offset = 1;
+			while (offset > 0) {
+				if (position.line - offset < 0) {
+					break;
+				}
+				
+				const lineBefore = document.lineAt(position.line - offset).text;
+				const matches = lineBefore.match(regexp);
+				if (!matches) {
+					break;
+				}
+				offset++;
+
+				const start = matches[1];
+				const startInt = parseInt(start, 10);
+				const end = matches[2];
+				const endInt =
+					end === '*'
+						? document.lineAt(position.line).text.length
+						: parseInt(end, 10);
+				const flags = matches[3];
+				const completeBracketPairs = flags.includes('b');
+				const isSnippet = flags.includes('s');
+				const text = matches[4].replace(/\\n/g, '\n');
+
+				result.items.push({
+					insertText: isSnippet ? new vscode.SnippetString(text) : text,
+					range: new Range(position.line, startInt, position.line, endInt),
+					completeBracketPairs,
+				});
+			}
+
+			if (result.items.length > 0) {
+				result.commands!.push({
+					command: 'demo-ext.command1',
+					title: 'My Inline Completion Demo Command',
+					arguments: [1, 2],
+				});
+			}
+			return result;
+		},
+
+		handleDidShowCompletionItem(completionItem: vscode.InlineCompletionItem): void {
+			console.log('handleDidShowCompletionItem');
+		},
+
+		/**
+		 * Is called when an inline completion item was accepted partially.
+		 * @param acceptedLength The length of the substring of the inline completion that was accepted already.
+		 */
+		handleDidPartiallyAcceptCompletionItem(
+			completionItem: vscode.InlineCompletionItem,
+			acceptedLength: number
+		): void {
+			console.log('handleDidPartiallyAcceptCompletionItem');
+		},
+	};
+	vscode.languages.registerInlineCompletionItemProvider({ pattern: '**' }, provider);
+}
